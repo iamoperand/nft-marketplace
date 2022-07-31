1b26e917c455240dbb5a94e9db32db9b2a2875a9
nikhil arora <narora200@gmail.com>
2022-07-31 13:34:28 +0530

    feat: ability to buy a listed nft

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -7,15 +7,22 @@ error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedForMarketplace();
 error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
 error NftMarketplace__NotOwner();
+error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
 
 contract NftMarketplace {
-    ////////////////////
+    ///////////////////////
     // Type Declarations //
-    ////////////////////
+    ///////////////////////
+
     struct Listing {
         uint256 price;
         address seller;
     }
+
+    ////////////
+    // Events //
+    ////////////
 
     event ItemListed(
         address indexed seller,
@@ -24,12 +31,22 @@ contract NftMarketplace {
         uint256 price
     );
 
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
     // NFT Contract Address => NFT Token ID => Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
 
-    ////////////////////
+    // Seller address -> Amount earned
+    mapping(address => uint256) private s_proceeds;
+
+    ///////////////
     // Modifiers //
-    ////////////////////
+    ///////////////
 
     modifier notListed(
         address nftAddress,
@@ -52,6 +69,14 @@ contract NftMarketplace {
         address owner = nft.ownerOf(tokenId);
         if (spender != owner) {
             revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NftMarketplace__NotListed(nftAddress, tokenId);
         }
         _;
     }
@@ -87,5 +112,26 @@ contract NftMarketplace {
 
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(address nftAddress, uint256 tokenId)
+        external
+        payable
+        isListed(nftAddress, tokenId)
+    {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (msg.value < listing.price) {
+            revert NftMarketplace__PriceNotMet(nftAddress, tokenId, listing.price);
+        }
+
+        /*
+         * Pull over Push pattern: https://fravoll.github.io/solidity-patterns/pull_over_push.html
+         * ❌ Sending the money to the user
+         * ✅ Have them withdraw the money
+         */
+        s_proceeds[listing.seller] = s_proceeds[listing.seller] + msg.value;
+        delete (s_listings[nftAddress][tokenId]);
+        IERC721(nftAddress).safeTransferFrom(listing.seller, msg.sender, tokenId);
+        emit ItemBought(msg.sender, nftAddress, tokenId, listing.price);
     }
 }
